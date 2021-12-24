2e681725a7c8492567da2c94d75384a63578777b
Hichahy <75397284+Hichahy@users.noreply.github.com>
2021-12-24 10:06:03 +0100

    small grind

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
+import './index.scss'
+import App from './App'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
-import './index.scss'
-import { Provider } from 'react-redux'
 import store from './store'
-import App from './App'
 
 ReactDOM.render(
   <Provider store={store}>
